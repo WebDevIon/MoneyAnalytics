ef466cb6cd4066c81333566f7cfc76f2c08d7869
Alin Simon <simon.alin90@gmail.com>
2018-08-08 12:48:36 +0300

    feat: Added Detail Activity and three fragments for income, expenses and savings drawer options.

---
+++ app/src/main/java/com/example/android/moneyanalytics/ui/DetailActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.moneyanalytics.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.example.android.moneyanalytics.ExpensesFragment;
+import com.example.android.moneyanalytics.R;
+import com.example.android.moneyanalytics.SavingsFragment;
+
+/**
+ * Detail activity that displays the correct fragment according to the user selection.
+ */
+public class DetailActivity extends AppCompatActivity implements
+        IncomeFragment.OnFragmentInteractionListener,
+        ExpensesFragment.OnFragmentInteractionListener,
+        SavingsFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Enable the up button to navigate up to the Main Activity.
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Get the intent extra which will be used to launch the correct fragment.
+        Intent intent = getIntent();
+        if (intent != null) {
+            String fragmentName = intent.getStringExtra(MainActivity.DETAIL_ACTIVITY_KEY);
+            Toast.makeText(this, fragmentName + " was clicked!", Toast.LENGTH_LONG).show();
+
+            // Set the title of the activity accordingly to the option that was selected by the user.
+            setTitle(fragmentName);
+
+            // Here we launch the fragment for the option that was selected by the user.
+            if (fragmentName.equals(getString(R.string.nav_drawer_income_string))) {
+                IncomeFragment incomeFragment = IncomeFragment.newInstance(fragmentName, "hello");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.detail_activity_frame_layout, incomeFragment)
+                        .commit();
+            } else if (fragmentName.equals(getString(R.string.nav_drawer_expenses_string))) {
+                ExpensesFragment expensesFragment = ExpensesFragment.newInstance(fragmentName, "hello");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.detail_activity_frame_layout, expensesFragment)
+                        .commit();
+            } else if (fragmentName.equals(getString(R.string.nav_drawer_savings_string))) {
+                SavingsFragment savingsFragment = SavingsFragment.newInstance(fragmentName, "hello");
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.detail_activity_frame_layout, savingsFragment)
+                        .commit();
+            }
+
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
--- app/src/main/java/com/example/android/moneyanalytics/ui/MainActivity.java
+++ app/src/main/java/com/example/android/moneyanalytics/ui/MainActivity.java
@@ -1,6 +1,8 @@
-package com.example.android.moneyanalytics;
+package com.example.android.moneyanalytics.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,8 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.example.android.moneyanalytics.R;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String DETAIL_ACTIVITY_KEY = "detail activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +25,19 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Here we initialize the drawer and we handle it's states (open or closed)
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    /**
+     * Method used to close the Drawer if back is pressed.
+     */
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -39,17 +48,38 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * Method responsible for clicking on items from the Navigation Drawer.
+     * For income, expenses and savings Detail Activity is launched and a string is passed
+     * which is the retrieved in the activity so that it displays the proper fragment.
+     * When a tracking period is clicked the Database will be queried accordingly to the user
+     * selection.
+     * For about the About Activity will vbe launched.
+     * @param item the item that was clicked
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_income) {
 
+            Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+            intent.putExtra(DETAIL_ACTIVITY_KEY, getString(R.string.nav_drawer_income_string));
+            startActivity(intent);
+
         } else if (id == R.id.nav_expenses) {
 
+            Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+            intent.putExtra(DETAIL_ACTIVITY_KEY, getString(R.string.nav_drawer_expenses_string));
+            startActivity(intent);
+
         } else if (id == R.id.nav_savings) {
+
+            Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+            intent.putExtra(DETAIL_ACTIVITY_KEY, getString(R.string.nav_drawer_savings_string));
+            startActivity(intent);
 
         } else if (id == R.id.nav_today) {
 
