d89ccc37843914ccb29738314229227687ad11ec
Alin Simon <simon.alin90@gmail.com>
2018-08-29 20:17:05 +0300

    feat: Created widget layout.

--- app/src/main/java/com/example/android/moneyanalytics/widget/MoneyAnalyticsWidget.java
+++ app/src/main/java/com/example/android/moneyanalytics/widget/MoneyAnalyticsWidget.java
@@ -1,9 +1,11 @@
-package com.example.android.moneyanalytics;
+package com.example.android.moneyanalytics.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
+
+import com.example.android.moneyanalytics.R;
 
 /**
  * Implementation of App Widget functionality.
